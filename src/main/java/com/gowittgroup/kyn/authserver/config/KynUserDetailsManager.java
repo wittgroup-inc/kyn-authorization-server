3b3f4f389adc585a3e28acceb248c561d564a6b4
Pawan Gupta <pawankgupta.se@gmail.com>
2023-04-13 19:47:30 +0530

    Changes to fix authorization bug.

--- src/main/java/com/gowittgroup/kyn/authserver/config/KynUserDetailsManager.java
+++ src/main/java/com/gowittgroup/kyn/authserver/config/KynUserDetailsManager.java
@@ -12,13 +12,14 @@ import org.springframework.security.core.userdetails.UserDetailsPasswordService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.provisioning.UserDetailsManager;
 
-import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
 public class KynUserDetailsManager implements UserDetailsManager, UserDetailsPasswordService {
     private final UserService userService;
+
     @Override
     public UserDetails updatePassword(UserDetails user, String newPassword) {
         return null;
@@ -48,52 +49,26 @@ public class KynUserDetailsManager implements UserDetailsManager, UserDetailsPas
     public boolean userExists(String username) {
         return true;
     }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userService.loadUser(username);
-        if (user != null) return toUserDetails(user);
-        throw new UsernameNotFoundException("Not found : " + username);
+
+        List<String> roles = List.of("USER");
+
+        if (user != null) {
+
+
+            return new org.springframework.security.core.userdetails.User(
+                    user.getEmail(),
+                    user.getPassword(),
+                    roles.stream()
+                            .map(role -> new SimpleGrantedAuthority(role))
+                            .collect(Collectors.toSet())
+
+            );
+        } else {
+            throw new UsernameNotFoundException("Not found : " + username);
+        }
     }
-
-
-    public UserDetails toUserDetails(final User user){
-        return  new UserDetails() {
-
-            @Override
-            public Collection<? extends GrantedAuthority> getAuthorities() {
-                return List.of(new SimpleGrantedAuthority("USER"));
-            }
-
-            @Override
-            public String getPassword() {
-                return user.getPassword();
-            }
-
-            @Override
-            public String getUsername() {
-                return user.getUserName();
-            }
-
-            @Override
-            public boolean isAccountNonExpired() {
-                return true;
-            }
-
-            @Override
-            public boolean isAccountNonLocked() {
-                return true;
-            }
-
-            @Override
-            public boolean isCredentialsNonExpired() {
-                return true;
-            }
-
-            @Override
-            public boolean isEnabled() {
-                return true;
-            }
-        };
-    }
-
 }
